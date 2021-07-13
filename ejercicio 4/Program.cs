e57e3ab642e95a2a1713421ff623628749ce9dc4
Alvaro <agalanteperez@gmail.com>
2021-07-13 11:36:15 +0200

    Initial commit

---
+++ ejercicio 4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ejercicio_4
+{
+    class Program
+    {
+        static void Main()
+        {
+            int N = 76;
+            Console.WriteLine($"Valor inicial de N es igual a {N}");
+            N += 77;
+            Console.WriteLine($" N + 77 = {N }");
+            N -= 3;
+            Console.WriteLine($" N - 3 = {N}");
+            N *= 2;
+            Console.WriteLine($" N * 2 = {N}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
